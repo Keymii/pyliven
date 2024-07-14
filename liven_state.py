4126afe32cd8476d5f2a75ba2ac1addc065d7d41
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2024-07-15 00:54:02 +0530

    UPDATE filenames

--- liven_state.py
+++ liven_state.py
@@ -1,40 +1,40 @@
-class PlumState:
-        
-    def __init__(self, plum, pointer, mulfac=1, addfac=0):
+class LivenState:
+    
+    def __init__(self, liven_box, pointer, mulfac=1, addfac=0):
         self.pointer = pointer
         self.mulfac  = mulfac
         self.addfac  = addfac
-        self.plum = plum
+        self.liven_box = liven_box
         
     def __add__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac + other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac + other)
     def __radd__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac + other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac + other)
     def __sub__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac - other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac - other)
     def __rsub__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac - other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac, addfac= self.addfac - other)
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac*other, addfac= self.addfac * other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac*other, addfac= self.addfac * other)
     def __rmul__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac*other, addfac= self.addfac * other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac*other, addfac= self.addfac * other)
     def __div__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac/other, addfac= self.addfac/other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac/other, addfac= self.addfac/other)
     def __rdiv__(self, other):
         if isinstance(other, (int, float)):
-            return PlumState(plum=self.plum, pointer=self.pointer, mulfac=self.mulfac/other, addfac= self.addfac/other)
+            return LivenState(liven_box=self.liven_box, pointer=self.pointer, mulfac=self.mulfac/other, addfac= self.addfac/other)
     
     def value(self):
-        return self.mulfac * self.plum.memory[self.pointer] + self.addfac
+        return self.mulfac * self.liven_box.memory[self.pointer] + self.addfac
     
-class Plum:
+class LivenBox:
     def __init__(self):
         self.memory = {}
 
@@ -42,7 +42,7 @@ class Plum:
         if name in self.memory:
             raise ValueError("Variable already exists")
         self.memory[name] = value
-        return PlumState(plum=self, pointer=name)
+        return LivenState(liven_box=self, pointer=name)
         
     def update(self, name, value):
         if name not in self.memory:
@@ -52,15 +52,15 @@ class Plum:
     def getVar(self, name):
         if name not in self.memory:
             raise ValueError("Variable does not exist")
-        return PlumState(plum=self, pointer=name)
+        return LivenState(liven_box=self, pointer=name)
     
         
 if __name__ == "__main__":
-    # you initialize a plum
-    plum = Plum()
+    # you initialize a liven_box
+    liven_box = LivenBox()
     
     # you create a variable a with value 10
-    a = plum.create(name='a', value= 10)
+    a = liven_box.create(name='a', value= 10)
     
     # you create a variable b with dependent on a
     b =  3*a + 4
@@ -69,7 +69,7 @@ if __name__ == "__main__":
     print(b.value())
     
     # you update the value of a
-    plum.update('a', 20)
+    liven_box.update('a', 20)
     
     # and you notice that the value of b also changes
     print(b.value())
