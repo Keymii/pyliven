958b751751416c97a66ae8c3ce03247b057dd456
Abhijat Bharadwaj <96429535+Keymii@users.noreply.github.com>
2024-07-17 23:28:37 +0530

    UPDATE liven.py and expectations.py

--- pyliven/exceptions.py
+++ pyliven/exceptions.py
@@ -2,3 +2,23 @@ class IllegalOperationError(Exception):
     """Raised when an illegal operation is performed on LiveNum"""
     def __init__(self):
         super().__init__("Arithmetic operation on LiveNum only permitted with int, float and LiveNum objects")
+
+class ImproperCoreValueError(Exception):
+    """Raised when an improper core value is passed to LiveNum"""
+    def __init__(self):
+        super().__init__("Core value must be a number or another LiveNum object")
+        
+class ImproperSideValueError(Exception):
+    """Raised when an improper side value is passed to LiveNum"""
+    def __init__(self):
+        super().__init__("Side must be a number, another LiveNum object, or None")
+        
+class MissingOperatorOrSideError(Exception):
+    """Raised when operator or side is not provided together"""
+    def __init__(self):
+        super().__init__("Operator and Side must be provided together")
+    
+class ImproperUpdateValueError(Exception):
+    """Raised when an improper value is passed to update method"""
+    def __init__(self):
+        super().__init__("Update value must be a number (int or float)")
--- pyliven/liven.py
+++ pyliven/liven.py
@@ -1,27 +1,40 @@
-from exceptions import IllegalOperationError
-class LiveNum():
+from pyliven.exceptions import *
+from typing import Type
+# from __future__ import annotations
+from typing import Optional, Union, Any
+
+
+class LiveNum():    
+    """LiveNum is a stateful class that allows you to create a number whose updation will update past calculations."""
+    size : int = 0
+    memory : dict[int, dict[str, Any]]= {}
     
-    size = 0
-    memory = {}
-    
-    def __init__(self, core, pow = 1, ncoeff = 1, dcoeff=1, add =0, ) -> None:
+    # User API functions
+    def __init__(self, core: Union[int,float,'LiveNum'], side : Optional[Union[int,float,'LiveNum']] = None , operator : Optional[int] = None) -> None:
+        
+        """LiveNum is a stateful class that allows you to create a number whose updation will update past calculations.        
+        
+        Operator Table:
+            0: Addition
+            1: Subtraction
+            2: Multiplication
+            3: Division
+            4: Floor Division 
+            5. Modulo
+            6. Power"""
+        
         if not isinstance(core, (int, float, LiveNum)):
-            raise TypeError("Core must be a number or another LiveNum object")
-        if not isinstance(pow, (int, float, LiveNum)):
-            raise TypeError("Pow must be a number or another LiveNum object")
-        if not isinstance(add, (int, float, LiveNum)):
-            raise TypeError("Add must be a number or another LiveNum object")
-        if not isinstance(ncoeff, (int, float, LiveNum)):
-            raise TypeError("nCoeff must be a number or another LiveNum object")
-        if not isinstance(dcoeff, (int, float, LiveNum)):
-            raise TypeError("dCoeff must be a number or another LiveNum object")
-    
+            raise ImproperCoreValueError
+        if not isinstance(side, (int, float, LiveNum, type(None))):
+            raise ImproperSideValueError
+        
+        if (operator or side) is not None and (operator and side) is None:
+            raise MissingOperatorOrSideError
         
         self._core = core 
-        self._pow = pow 
-        self._ncoeff = ncoeff 
-        self._dcoeff = dcoeff 
-        self._add = add
+        self._side = side
+        self._operator = operator
+        
         self._id = LiveNum.size
         LiveNum.size+=1
         LiveNum.memory[self._id] = {
@@ -31,85 +44,52 @@ class LiveNum():
             }
         self.value()
         
-        params = [core, pow, ncoeff, dcoeff, add]        
+        params = [core, side]        
         self._depends_on = []
         for i in params:
             if isinstance(i, LiveNum):
                 self._depends_on.append(i)
                 LiveNum.memory[i._id]['dependants'].append(self)
-        
+                    
     def value(self, force=False):
+        """Returns the value of the LiveNum object. If force is set to True, it will recalculate the value of the object."""
+        
         if force or (LiveNum.memory[self._id]['cache'] is None):
             core = self._core if isinstance(self._core, (int, float)) else self._core.value()
-            power = self._pow if isinstance(self._pow, (int, float)) else self._pow.value()
-            ncoeff = self._ncoeff if isinstance(self._ncoeff, (int, float)) else self._ncoeff.value()
-            dcoeff = self._dcoeff if isinstance(self._dcoeff, (int, float)) else self._dcoeff.value()
-            add = self._add if isinstance(self._add, (int, float)) else self._add.value()
-            val = (core**power)*ncoeff/dcoeff+add
+            if self._side is None:
+                val = core
+                LiveNum.memory[self._id]['cache'] = val
+                return val
+            side = self._side if isinstance(self._side, (int, float)) else self._side.value()
+            val = None
+            if self._operator == 0:
+                val = core + side
+            elif self._operator == 1:
+                val = core - side
+            elif self._operator == 2: 
+                val = core * side
+            elif self._operator == 3:
+                val = core / side
+            elif self._operator == 4:
+                val = core // side
+            elif self._operator == 5:
+                val = core % side
+            elif self._operator == 6:
+                val = core ** side
             LiveNum.memory[self._id]['cache'] = val
             return val
         else:
             return LiveNum.memory[self._id]['cache']
-    
-    def __add__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=self, add=other)
-    def __radd__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=other, add=self)
-    def __neg__(self):
-        return LiveNum(core=self, ncoeff=-1)
-    def __sub__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=self, add=-other)
-    def __rsub__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=other, add=-self)
-    def __mul__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=self, ncoeff=other)
-    def __rmul__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=other, ncoeff=self)
-    def __truediv__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=self, dcoeff=other)
-    def __rtruediv__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=other, dcoeff=self)
-    def __pow__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=self, pow=other)
-    def __rpow__(self, other):
-        if not isinstance(other, (int, float, LiveNum)):
-            raise IllegalOperationError
-        return LiveNum(core=other, pow=self)
-    
-    def __repr__(self) -> str:
-        return str(self.value())
-    def __str__(self) -> str:
-        return str(self.value())
-    
-    def __int__(self):
-        return int(self.value())
-    def __float__(self):
-        return float(self.value())
-    
+        
     def update(self, value):
+        """Updates the value of the LiveNum object."""
+        if not isinstance(value, (int, float)):
+            raise ImproperUpdateValueError
+        
         self._core = value
-        self._add = 0
-        self._pow = 1
-        self._ncoeff = 1
-        self._dcoeff = 1
+        self._side = None
+        self._operator = None
+        
         for i in self._depends_on:
             LiveNum.memory[i._id]['dependants'].remove(self)
         self._depends_on = []
@@ -121,14 +101,77 @@ class LiveNum():
         for i in LiveNum.memory[self._id]['dependants']:
             LiveNum.memory[self._id]['dependants'].extend( LiveNum.memory[i._id]['dependants'] )
             i.value(force=True)
+
+    # Arithmetic Operations
+    def __add__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=0)
+    def __radd__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=0)
+    def __neg__(self):
+        return LiveNum(core=self, ncoeff=-1)
+    def __sub__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=1)
+    def __rsub__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=1)
+    def __mul__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=2)
+    def __rmul__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=2)
+    def __truediv__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=3)
+    def __rtruediv__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=3)
+    def __floordiv__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=4)
+    def __rfloordiv__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=4)
+    def __mod__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=5)
+    def __rmod__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=5)
+    def __pow__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=self, side=other, operator=6)
+    def __rpow__(self, other):
+        self._checkIllegalOperation(other)
+        return LiveNum(core=other, side=self, operator=6)
     
-k = 9e9
-q1 = (2e-3)
-q2 = (3e-3)
+    # Private Helper Functions
+    def _checkIllegalOperation(self, other):
+        if not isinstance(other, (int, float, LiveNum)):
+            raise IllegalOperationError
+    
+    # Representation and Type Conversions
+    def __repr__(self) -> str:
+        return str(self.value())
+    def __str__(self) -> str:
+        return str(self.value())
+    def __int__(self):
+        return int(self.value())
+    def __float__(self):
+        return float(self.value())
 
-r = LiveNum(1)
+if __name__ == "__main__":
+    k = 9e9
+    q1 = LiveNum(2e-3)
+    q2 = (3e-3)
 
-f = k*q1*q2/r**2
-for i in range(1,11):
-    r.update(i)
-    print(f)
+    r = LiveNum(1)
+
+    f = k*q1*q2/r**2
+    for i in range(1,11):
+        r.update(i)
+        q1.update(q1.value()-i*1e-5)
+        print(f)
